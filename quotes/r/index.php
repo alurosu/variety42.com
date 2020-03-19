e5000b3a7cb8b1ab4a5e7fe2103dcf652a996792
alurosu <alurosu@gmail.com>
2020-03-19 13:57:15 +0200

    - improved random based on tag

--- quotes/r/index.php
+++ quotes/r/index.php
@@ -6,7 +6,13 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT id FROM content ORDER BY RAND() LIMIT 1";
+$sql = "SELECT t.id as id FROM content c, tags t, content_tags ct WHERE c.id = ct.content_id AND t.id = ct.tag_id GROUP BY t.id HAVING count(c.id) > 10 ORDER BY RAND() LIMIT 1";
+$result = $conn->query($sql);
+if ($result->num_rows > 0) {
+    $tag = $result->fetch_assoc();
+}
+
+$sql = "SELECT c.id FROM content c, content_tags ct, tags t WHERE c.id = ct.content_id AND t.id = ct.tag_id AND t.id = ".$tag['id']." ORDER BY RAND() LIMIT 1";
 $result = $conn->query($sql);
 if ($result->num_rows > 0) {
     $raw = $result->fetch_assoc();
