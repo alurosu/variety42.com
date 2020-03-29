08d29f9882d5fd3cbc213120eabf1c85bd397be4
alurosu <alurosu@gmail.com>
2020-03-29 13:41:17 +0300

    - edit function

---
+++ data/js/user.js
@@ -0,0 +1,57 @@
+jQuery(document).ready(function(){
+    console.log('user.js: loaded!');
+
+    jQuery('.user form .tags li').click(function(){
+        jQuery(this).remove();
+    });
+
+    jQuery('.user form .suggested_tag').on('keypress',function(e) {
+        if(e.which == 13) {
+            var suggested_tag = '<li>'+jQuery(this).val()+' <span>x</span><input type="hidden" name="tag_names[]" value="'+jQuery(this).val()+'"><input type="hidden" name="tag_ids[]" value="'+getIDfromSuggestedTags(jQuery(this).val())+'"></li>';
+            jQuery('.user form .tags').append(suggested_tag);
+            jQuery(this).val('');
+            e.preventDefault();
+        }
+    });
+
+    jQuery('.user form .autocomplete').on("click","li",function(){
+        var suggested_tag = '<li>'+jQuery(this).html()+' <span>x</span><input type="hidden" name="tag_names[]" value="'+jQuery(this).html()+'"><input type="hidden" name="tag_ids[]" value="'+jQuery(this).data('id')+'"></li>';
+        jQuery('.user form .tags').append(suggested_tag);
+        jQuery(this).remove();
+    });
+
+    jQuery('.user form .suggested_tag').on('keyup',function(e) {
+        jQuery('.user form .autocomplete').html('');
+        if(jQuery(this).val().length > 2){
+            var url = "/data/php/getSuggestedTags.php?folder="+jQuery(this).data('folder')+'&keyword='+jQuery(this).val();
+            jQuery.get(url, function(data){
+                suggested_tags = '';
+                if(data.suggestions!=null && data.suggestions.length > 0){
+                    for( row of data.suggestions ){
+                        if(!isSuggestedIDinList(row.id))
+                            suggested_tags += '<li data-id="'+row.id+'">'+row.name+'</li>';
+                    }
+                    jQuery('.user form .autocomplete').html(suggested_tags);
+                    jQuery(this).val('');
+                }
+
+            });
+        }
+    });
+});
+function isSuggestedIDinList(id){
+    var result = false;
+    jQuery('.user form .tags li').each(function(){
+        if(jQuery(this).find('input[name="tag_ids[]"]').val() == id)
+            result = true;
+    });
+    return result;
+}
+function getIDfromSuggestedTags(name){
+    var result = '';
+    jQuery(".user form .autocomplete li").each(function(){
+        if( name.toLowerCase() == jQuery(this).html().toLowerCase() )
+            result = jQuery(this).data('id');
+    });
+    return result;
+}
---
+++ u/edit.php
@@ -0,0 +1,115 @@
+<?php
+$folder = $_GET['folder'];
+if (!file_exists("../".$folder."/config.php"))
+    $folder = "jokes";
+
+$id = 1;
+if (is_numeric($_GET['id']) && $_GET['id'] > 0)
+    $id = $_GET['id'];
+require_once("../".$folder."/config.php");
+
+$conn = new mysqli($config->mysql->host, $config->mysql->user, $config->mysql->pass, $config->mysql->name);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+$message = '';
+if(isset($_POST['edit'])){
+    if(strlen($_POST['text'])>0){
+        $text = $conn->real_escape_string($_POST['text']);
+        $sql ="UPDATE content SET text = '$text' WHERE id = '$id'";
+        $conn->query($sql);
+
+        $sql_insert_tags = "INSERT INTO content_tags (content_id, tag_id) VALUES";
+        //add tags that don't exist
+        $tag_names = $_POST['tag_names'];
+        $tag_ids = $_POST['tag_ids'];
+        for($i=0;$i<count($tag_names);$i++){
+            if(!$tag_ids[$i]){
+                $sql = "INSERT INTO tags (name) VALUES ('".$tag_names[$i]."')";
+                $conn->query($sql);
+                $tag_ids[$i] = $conn->insert_id;
+            }
+            $sql_insert_tags.=" ('$id','".$tag_ids[$i]."')";
+            if($i!=(count($tag_names)-1))
+                $sql_insert_tags.=",";
+        }
+
+        //delete all content tags
+        $sql = "DELETE FROM content_tags WHERE content_id = $id";
+        $conn->query($sql);
+
+        //add tags to content
+        $conn->query($sql_insert_tags);
+
+        $message = 'Done! <a href="'.$config->single.'/'.$id.'" target="_blank"> Click Here </a>';
+    }
+}
+?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<title>Edit <?php echo $folder." - id: ".$id;?></title>
+
+	<meta property="og:title" content="Edit <?php echo $folder." - id: ".$id;?>">
+	<meta property="og:type" content="website" />
+	<meta property="og:description" content="User Panel">
+	<meta property="og:image" content="https://variety42.com<?php echo $config->folder; ?>/variety42_share.jpg">
+	<meta property="fb:app_id" content="616440488939771"/>
+
+	<link rel="shortcut icon" href="/favicon.png" />
+	<link rel="stylesheet" href="/data/css/main.css" />
+	<link rel="stylesheet" href="/data/css/user.css" />
+
+	<script type="text/javascript" src="/data/js/jquery.js"></script>
+	<script type="text/javascript" src="/data/js/main.js"></script>
+	<script type="text/javascript" src="/data/js/user.js"></script>
+
+    <?php require_once('../data/php/header.php'); ?>
+</head>
+
+<body>
+    <?php require_once("../".$folder."/data/php/menu.php"); ?>
+    <div class="wrapper user">
+        <h3>Edit <?php echo $folder." - id: ".$id;?><div class="edit_notification"> <?php echo $message; ?></div></h3>
+        <?php
+        $sql = "SELECT text FROM content WHERE id = ".$id;
+        $result = $conn->query($sql);
+        if ($result->num_rows > 0) {
+            $single = $result->fetch_assoc();
+            ?>
+            <form action="" method="post">
+                <textarea name="text"><?php echo $single['text'];?></textarea>
+                <ul class="tags">
+                    <?php
+                    $sql = 'SELECT t.name as name, t.id as id FROM content_tags ct, tags t WHERE ct.tag_id = t.id AND ct.content_id = '.$id;
+                    $result = $conn->query($sql);
+
+                    if ($result->num_rows > 0) {
+                        // output data of each row
+                        while($tag = $result->fetch_assoc()) { ?>
+                            <li>
+                                <?php echo $tag['name']; ?>
+                                <span>x</span>
+                                <input type="hidden" name="tag_names[]" value="<?php echo $tag['name'];?>">
+                                <input type="hidden" name="tag_ids[]" value="<?php echo $tag['id'];?>">
+                            </li>
+                        <?php }
+                    }
+                    ?>
+                </ul>
+                <input type="text" class="suggested_tag" data-folder="<?php echo $folder; ?>" placeholder="Add tag" value="">
+                <ul class="autocomplete"></ul>
+                <input type="submit" name="edit" value="Edit">
+            </form>
+        <?php } else echo "There are no ".$folder." with id: ".$id; ?>
+    </div>
+
+    <?php require_once('../data/php/footer.php'); ?>
+</body>
+</html>
+<?php $conn->close(); ?>
